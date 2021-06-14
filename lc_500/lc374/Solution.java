85db22d3b7565b0f52a05157d7e0d625065b1722
qingjiusanliangsan <2394753336@qq.com>
2021-06-14 11:01:08 +0800

    Signed-off-by: qingjiusanliangsan <2394753336@qq.com>

---
+++ lc_500/lc374/Solution.java
@@ -0,0 +1,40 @@
+package lc_500.lc374;
+/**
+ * @author qingjiusanliangsan
+ * create 2021-06-14-10:48
+ * lc374.猜数字大小
+ */
+
+
+public class Solution {
+    /**
+     * Forward declaration of guess API.
+     * @param  num   your guess
+     * @return 	     -1 if num is lower than the guess number
+     *			      1 if num is higher than the guess number
+     *               otherwise return 0
+     * int guess(int num);
+     */
+    public int guess(int num){
+        return 0;
+    }
+    public int guessNumber(int n) {
+        int a = 1;
+        int b = n;
+        int c = 0;
+        while(a<b){
+            c = a +(b-a)/2;
+            if(guess(c)==0){
+                return c;
+            }
+            else if(guess(c)==-1){
+                b = c-1;
+
+            }
+            else if(guess(c)==1){
+                a = c+1;
+            }
+        }
+        return a;
+    }
+}
