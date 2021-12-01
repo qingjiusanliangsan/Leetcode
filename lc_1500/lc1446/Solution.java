c11ac3d1bcd506025c5ed660bcb788ad0a04d76a
qingjiusanliangsan <2394753336@qq.com>
2021-12-01 16:08:45 +0800

    2021_12_1

---
+++ lc_1500/lc1446/Solution.java
@@ -0,0 +1,17 @@
+package lc_1500.lc1446;
+
+public class Solution {
+    public int maxPower(String s) {
+        int res = 1,n=s.length(),l=1;
+        for(int i=1;i<n;i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                ++l;
+            }
+            else{
+                res = Math.max(res,l);
+                l=1;
+            }
+        }
+        return Math.max(res,l);
+    }
+}
