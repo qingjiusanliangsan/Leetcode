bed006a3b1ffecf040f667b01e5c7579eb65e8a3
qingjiusanliangsan <2394753336@qq.com>
2021-12-28 21:42:48 +0800

    2021-12-28_1

--- lc_500/lc481/Solution.java
+++ lc_500/lc481/Solution.java
@@ -1,43 +1,8 @@
 package lc_500.lc481;
 
-
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class Solution {
     public int magicalString(int n) {
-        if(n==1){
-            return 1;
-        }
-        int res=0;
-        int a=1,b=2,las=1,now;
-        Queue<Integer> Lnum = new ArrayDeque<>();
-        Lnum.add(1);
-        Lnum.add(2);
-        while (b<n){
-            now=Lnum.poll();
-            Lnum.add(2/las);
-            if(now==1){
-                ++res;
-            }
-            else{
-                Lnum.add(2/las);
-                ++b;
-            }
-            las = 2/las;
-            ++a;
-            ++b;
-        }
-        while (!Lnum.isEmpty()){
-            now=Lnum.poll();
-            if(now==1){
-                ++res;
-            }
-        }
-        return res;
-    }
-    public int magicalString1(int n) {
-        if(n==1){
+        if(n<=3){
             return 1;
         }
         int res=1;
@@ -47,11 +12,11 @@ public class Solution {
         num[1]=2;
         while (b<n){
             if(num[a]==1){
-                ++res;
                 num[b] = (2/num[b-1]);
                 b++;
+                ++res;
             }
-            else if(num[a]==2){
+            else{
                 num[b] = (2/num[b-1]);
                 b++;
                 num[b] = (2/num[b-2]);
