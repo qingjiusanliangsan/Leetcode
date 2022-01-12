4c27c60ccee04c30e87c7b586fef68a5bb18638c
qingjiusanliangsan <2394753336@qq.com>
2022-01-12 23:33:23 +0800

    2022-01-12

---
+++ lc_500/lc334/Solution.java
@@ -0,0 +1,28 @@
+package lc_500.lc334;
+
+/**
+ * @author qingjiusanliangsan
+ * create 2022-01-12-23:18
+ */
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        if(n<3){
+            return false;
+        }
+        int[] arrmax = new int[n];
+        arrmax[n-1] = nums[n-1];
+        int a=nums[0];
+        for(int i=n-2;i>=0;--i){
+            arrmax[i] = Math.max(arrmax[i+1],nums[i]);
+        }
+        for(int i=1;i<n-1;i++){
+            a = Math.min(a,nums[i-1]);
+            if(nums[i]>a && nums[i]<arrmax[i+1]){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
