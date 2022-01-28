095bc750b65afa4b460002b64ffcac9f49365b35
qingjiusanliangsan <2394753336@qq.com>
2022-01-28 14:22:40 +0800

    2022-01-28

---
+++ lc_2000/lc2034/StockPrice.java
@@ -0,0 +1,42 @@
+package lc_2000.lc2034;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+/**
+ * @author qingjiusanliangsan
+ * create 2022-01-23-15:13
+ */
+public class StockPrice {
+    int maxTimestamp;
+    HashMap<Integer, Integer> timePriceMap;
+    TreeMap<Integer, Integer> prices;
+
+    public StockPrice() {
+        maxTimestamp = 0;
+        timePriceMap = new HashMap<Integer, Integer>();
+        prices = new TreeMap<Integer, Integer>();
+    }
+
+    public void update(int timestamp, int price) {
+        maxTimestamp = Math.max(maxTimestamp, timestamp);
+        int prevPrice = timePriceMap.getOrDefault(timestamp, 0);
+        timePriceMap.put(timestamp, price);
+        if (prevPrice > 0) {
+            prices.put(prevPrice, prices.get(prevPrice) - 1);
+            if (prices.get(prevPrice) == 0) {
+                prices.remove(prevPrice);
+            }
+        }
+        prices.put(price, prices.getOrDefault(price, 0) + 1);
+    }
+    public int current() {
+        return timePriceMap.get(maxTimestamp);
+    }
+    public int maximum() {
+        return prices.lastKey();
+    }
+    public int minimum() {
+        return prices.firstKey();
+    }
+}
