d584a429f42d72b173c633c9956a37a7765ef140
qingjiusanliangsan <2394753336@qq.com>
2022-03-10 15:40:19 +0800

    2022-3-10

--- lc_500/lc200/Solution.java
+++ lc_500/lc200/Solution.java
@@ -1,45 +1,31 @@
 package lc_500.lc200;
 
-public class Solution {
-    int tem = 0;
-    int n,m;
-    int[][] count;
+
+public class Solution{
+    int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
     public int numIslands(char[][] grid) {
-        n = grid.length;
-        m = grid[0].length;
-        count = new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                count[i][j] = 0;
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && count[i][j] == 0){
-                    count[i][j] = tem+1;
-                    make(grid,i,j,tem+1);
-                    ++tem;
+        int n = grid.length;
+        int m = grid[0].length;
+        int ans = 0;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(grid[i][j]=='1'){
+                    grid[i][j] = '2';
+                    make(i,j,n,m,grid);
+                    ++ans;
                 }
             }
         }
-        return tem;
+        return ans-2;
     }
-    public void make(char[][] grid,int a,int b,int k){
-        if(a-1>=0 && grid[a-1][b]=='1' && count[a-1][b] == 0){
-            count[a-1][b] = k;
-            make(grid,a-1,b,k);
-        }
-        if(b+1<m && grid[a][b+1]=='1' && count[a][b+1] == 0){
-            count[a][b+1] = k;
-            make(grid,a,b+1,k);
-        }
-        if(a+1<n && grid[a+1][b]=='1' && count[a+1][b] == 0){
-            count[a+1][b] = k;
-            make(grid,a+1,b,k);
-        }
-        if(b-1>=0 && grid[a][b-1]=='1' && count[a][b-1] == 0){
-            count[a][b-1] = k;
-            make(grid,a,b-1,k);
+    public void make(int a,int b,int n,int m,char[][] grid){
+        for(int i=0;i<4;++i){
+            int x = a+dir[i][0];
+            int y = b+dir[i][1];
+            if(x>=0 && x<n && y>=0&& y<m && grid[x][y]=='1'){
+                grid[x][y] = '2';
+                make(x,y,n,m,grid);
+            }
         }
     }
 }
