de9711e1fd005e8a4fbd0836bf12290893deb897
qingjiusanliangsan <2394753336@qq.com>
2022-03-30 10:32:10 +0800

    20220330

--- hot100/lc3/Solution.java
+++ hot100/lc3/Solution.java
@@ -1,9 +1,32 @@
 package hot100.lc3;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
         int res = 0;
-        int[] num = new int[26];
-
+        int n = s.length();
+        Set<Character> hset = new HashSet<>();
+        int R=0,L=0;
+        while (R < n){
+            while (R<n && !hset.contains(s.charAt(R))){
+                hset.add(s.charAt(R));
+                ++R;
+            }
+            res = Math.max(res,hset.size());
+            while (L < R && R<n){
+                if(s.charAt(L)!=s.charAt(R)){
+                    hset.remove(s.charAt(L));
+                    ++L;
+                }
+                else{
+                    ++L;
+                    break;
+                }
+            }
+            ++R;
+        }
+        return res;
     }
 }
